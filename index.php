bd808a93c3ff2e103d968e4b52d01b64cbee8751
Julian Sovernigo <jsovernigo@hotmail.com>
2018-08-24 08:18:01 -0400

    Init commit

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Bridgeport Rod and Gun</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom fonts for this template -->
+		<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+		<!-- Plugin CSS -->
+		<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="css/brg.css" rel="stylesheet">
+
+	</head>
+
+	<body id="page-top">
+
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+			<div class="container">
+				<a class="navbar-brand js-scroll-trigger" href="#page-top"><img src='img/brg.png' height=32 width=32></img>  Bridgeport Rod and Gun</a>
+
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#about">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#facilities">Facilities</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#events">Events</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#range">Range</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#membership">Membership</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+<!-- SECTION BEGIN MAIN-PAGE -->
+		<header class="masthead text-center text-white d-flex">
+			<div class="container my-auto">
+				<div class="row">
+					<div class="col-lg-4 mx-auto">
+						<p class="text-faded mb-5">"The wildlife of today is not ours to dispose of as we please. We have it in trust. We must account for it to those who come after."<br />--George VI</p>
+						<hr />
+						<a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<section class="bg-primary" id="about">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mx-auto text-center">
+						<h2 class="section-heading text-white">Dedicated to Conservation</h2>
+						<hr class="light my-4">
+						<p class="text-faded mb-4">Founded in 1950 and dedicated to conservation, the Bridgeport Rod and Gun Club is still one of the best clubs in the Waterloo Region. Our facilities include Trap and Skeet fields, Handgun, Rifle and Archery Ranges, three Baseball Diamonds (two with night lighting), and both indoor and outdoor seasonal Horse Shoes Pits. Card games and Darts can also be enjoyed in our clubhouse, where refreshments and food are available. Other activities include an annual Conservation Dinner, Fish Fry and Golf Tournament just to mention a few.</p>
+						<a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Become a Member</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+<!-- SECTION BEGIN FACILITIES -->
+		<section id="facilities">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2 class="section-heading">Facilities</h2>
+						<hr class="my-4">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="service-box mt-5 mx-auto">
+							<img src='img/baseball.png' width='64' height='64'></img>
+							<h3 class="mb-3">Baseball</h3>
+							<p class="text-muted mb-0">The club maintains 3 baseball diamonds, two of which are equipped with lights for games at night. Games are frequently played on weeknights and event weekends.</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="service-box mt-5 mx-auto">
+							<img src='img/shotgun.jpg' width='64' height='64'></img>
+							<h3 class="mb-3">Trap and Skeet</h3>
+							<p class="text-muted mb-0">Just behind the main hall, the trap house services two fields.  One for trap, wobble included, and one for skeet.  Please note that the 100m range is closed when skeet is being shot.</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="service-box mt-5 mx-auto">
+							<img src='img/rifle.png' height='64' width='64'></img>
+							<h3 class="mb-3">Rifles and Handguns</h3>
+							<p class="text-muted mb-0">For members and their guests, the club offers two ranges: one 25m range for rifles and handguns, and one 100m range for rifles only.</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="service-box mt-5 mx-auto">
+							<img src='img/horseshoe.png' height='64' width='64'></img>
+							<h3 class="mb-3">Horseshoes</h3>
+							<p class="text-muted mb-0">With indoor and outdoor horseshoe pits, the club offers horshoe space in any season. Many members play weekly.</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="service-box mt-5 mx-auto">
+							<!-- <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
+							-->
+							<img src='img/bowarrow.png' width='64' height='64'></img>
+							<h3 class="mb-3">Archery</h3>
+							<p class="text-muted mb-0">Open a half hour after sunrise to a half hour before sunset, the archery range is located on the south side of the property.</p>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 text-center">
+						<div class="service-box mt-5 mx-auto">
+							<!-- <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
+							-->
+							<img src='img/clubhouse.png' width='64' height='64'></img>
+							<h3 class="mb-3">Clubhouse</h3>
+							<p class="text-muted mb-0">Open every weeknight from 6pm to 9pm, the clubhouse has refreshments and food, and is a great space to have get-togethers or meetings.  Indoor horseshoe pits are located here, and dartboards are available.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+<!-- BEGIN SECTION EVENTS -->
+		<section id="events" class='bg-primary no-gutter pb-0 pt-auto'>
+			<div class="container p-0">
+				<div class="container-fluid p-0">
+					<div class='row'>
+						<div class='col-lg-8 mx-auto text-center'>
+							<h2 class="section-heading text-white">Events</h2>
+							<hr class='light my-4'>
+							<p class="text-faded mb-4">As members of the same club, we at Bridgeport Rod and Gun feel that community is essential. Our events therefore reflect our dedication to our organization and our community; from being considerate neighbours to homeowners down the road, to holding our annual Slow-Pitch For Autism tournament, we are determined to maintain our respectful community of members.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='container-fluid no-gutters mt-4 p-0 bg-dark'>
+				<div class='container no-gutters p-0 mx-auto bg-black'>
+					<div id="carouselControls" class="carousel slide" data-ride="carousel">
+  						<div class="my-auto carousel-inner">
+
+<?php
+
+$pictures = scandir('img/gallery/');
+
+// remove the back directories from the list.
+$pictures = array_diff($pictures, array('.', '..'));
+
+$i = 0;
+
+foreach ($pictures as $pic) {
+	$active = '';
+	if ($i == 0) {
+		$active = 'active';
+	}
+	echo <<<__EOL
+    						<div class="item carousel-item $active">
+      							<img class="d-block w-100 mx-auto" style='height: 900px !important; width: auto !important;' src='img/gallery/$pic' alt="$pic">
+    						</div>
+__EOL;
+	$i ++;
+}
+?>
+						</div>
+
+						<!-- CONTROLS -->
+						<a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+    						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    						<span class="sr-only">Previous</span>
+  						</a>
+  						<a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+    						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    						<span class="sr-only">Next</span>
+  						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+<!-- BEGIN SECTION RANGE -->
+		<section id='range'>
+			<div class="container text-center">
+				<h2 class="mb-4">The Range</h2>
+
+				<hr class='my-4'>
+
+				<h4>Please note that all shooting must be supervised by a club approved <b>Range Safety Officer</b></h4>
+				<p></p>
+				<a class="btn btn-light btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Range Calendar</a>
+			</div>
+		</section>
+
+
+<!-- BEGIN SECTION MEMBERSHIP -->
+		<section id='membership' class="bg-primary text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mx-auto text-center">
+						<h2 class="section-heading text-white">Membership</h2>
+						<hr class="light my-4">
+						<p class="text-faded mb-4">Becoming a member at Bridgeport Rod and Gun club is a simple process.  For most memberships, a valid Possession/Acquisition License (PAL) is needed. A link to our membership form can be found below. There are 5 steps to becoming a member.</p>
+						<h4>Steps to become a member</h4>
+						<div class='text-left' class='text-faded mb-4'>
+							<strong>1.  </strong>Download and fill out the apllication form.  Provided is a fillable PDF, which can be filled either electronically or using a pen.<br />
+<p>
+								<strong>2.  </strong>Bring your completed membership application to a board meeting on the first Tuesday of the month.  Note that to attend the meethings, you should arrive before 7:30pm.  If attendance of a meeting is impossible for you, you will need to obtain a sponsorship signature from two (2) members, or one (1) director, and drop off the completed application any time the club is open (weekdays 6pm-9pm, or event weekends).<br />
+								<strong>3.  </strong>You will need to attend a club orientation course, which will occur after any board meeting.<br />
+								<strong>4.  </strong>Every new member, regardless of skill or experience, must complete 3 probationary shoots and 4 hours of voluntary labour. The shoots must be completed on <i>each range</i>, the final of the three being a supervised shoot where your range safety will be evaluated.  This means you need one shoot on each of the trap/skeet, handgun, and rifle ranges.  Please note that only one signature may be obtained per day. <br />
+								<strong>5.  </strong>Once all four signatures have been obtained, you must drop off the card on any night where the clubhouse is staffed to the bartender, who will give you your full membership card.
+							</p>
+						</div>
+						<a class="btn btn-light btn-xl js-scroll-trigger" href="http://bit.ly/BRGApplicationFillable">Download Membership Form</a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+<!-- BEGIN SECTION CONTACT -->
+		<section id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mx-auto text-center">
+						<h2 class="section-heading">Contact Us</h2>
+						<hr class="my-4">
+						<p class="mb-5">Please note that the Club House is only staffed and telephone answered on <b> weekdays 6pm to 9pm</b> and event weekends.  Email is the most reliable method of communication.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 ml-auto text-center">
+						<i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
+						<p>+1 (519) 648-2633</p>
+					</div>
+					<div class="col-lg-4 mr-auto text-center">
+						<i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
+						<p>
+							<a href="mailto:contact@brgclub.com">contact@brgclub.com</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Bootstrap core JavaScript -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<!-- Plugin JavaScript -->
+		<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+		<script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+		<!-- Custom scripts for this template -->
+		<script src="js/creative.min.js"></script>
+
+
+	</body>
+</html>
